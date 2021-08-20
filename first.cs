4f686751b637fca22e418e14d014d683a3c5bd16
yahyehaidar <51471098+yahyehaidar@users.noreply.github.com>
2021-08-20 16:11:48 +0530

    first edit

--- first.cs
+++ first.cs
@@ -7,6 +7,8 @@ namespace yahye
         public static void Main(string[] args)
         {
             Console.WriteLine("this is my first CSharp Program here in GitHub");
+            
+            Console.WritLine("i'm editing my first CSharp Code Here in Github");
         }
      }
 }
