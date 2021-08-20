fe859b51e7c6d961b771828e14268393810a522c
yahyehaidar <51471098+yahyehaidar@users.noreply.github.com>
2021-08-20 16:09:20 +0530

    CSharp file created

---
+++ first.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace yahye
+{
+    class yahyeahmed
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("this is my first CSharp Program here in GitHub");
+        }
+     }
+}
+    
